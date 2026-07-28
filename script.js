367dcaf7d91bb34c26afbcd72caaabd352be4cef
shivakumarsk2026-ai <shivakumarsk2026@gmail.com>
2026-07-28 22:51:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const value = button.innerText;
+
+        if (value === "C") {
+            display.value = "";
+        }
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = "Error";
+            }
+        }
+        else {
+            display.value += value;
+        }
+
+    });
+});S
